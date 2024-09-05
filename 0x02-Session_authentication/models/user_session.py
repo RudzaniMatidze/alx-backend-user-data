cab093ef5856b63ba86012bd957e1f2d69034165
RudzaniMatidze <rdzmatidze@gmail.com>
2024-09-05 15:28:31 +0000

    fix bugs

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -11,6 +11,6 @@ class UserSession(Base):
     def __init__(self, *args: list, **kwargs: dict):
         """Initializes a User session instance
         """
-        super.__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_is')
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
