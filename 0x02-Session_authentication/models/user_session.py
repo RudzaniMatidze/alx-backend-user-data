3bb398ae2ba9b7f8a827c8140343ff9fabef7127
RudzaniMatidze <rdzmatidze@gmail.com>
2024-09-05 13:41:18 +0000

    Create a new model UserSession

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""User session module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """User session Class
+    """
+
+    def __init__(self, *args:list, **kwargs: dict):
+        """Initializes a User session instance
+        """
+        super.__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_is')
+        self.session_id = kwargs.get('session_id')
